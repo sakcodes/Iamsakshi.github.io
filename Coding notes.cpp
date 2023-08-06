e9a82965f0d66d40157ed94324d821e496df1c26
sakcodes <114860003+sakcodes@users.noreply.github.com>
2023-08-06 19:13:22 +0530

    Add files via upload

---
+++ Coding notes.cpp
@@ -0,0 +1,14 @@
+Coding notes
+
+1) When we say that an algorithm X is asymptotically more efficient than Y, we mean that as
+ the size of the input to the algorithm grows larger and larger, the running time of X 
+will eventually become faster than the running time of Y. This is typically expressed using 
+big O notation, which gives an upper bound on the worst-case running time of an algorithm.
+
+Vectors ka reverse ka syntax reverse(arr.begin(), arr.end())
+Array ko sort kar sakte hai sort (arr1[0], arr[n-1])
+i1 = std::max_element(arr, arr + n);
+
+max se 2 values ka max milta hai or  max_element se array ka max number without for loop 
+
+jab bhi return karna toh curly brackets laga kar karna 
